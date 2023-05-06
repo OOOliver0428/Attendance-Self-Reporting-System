d854a6fe68b47fa97ecbb9c09d134dcfb905f21e
HuangYu Sun <qq498429831@gmail.com>
2023-05-06 18:40:24 +0800

    完成登录和验证的基本功能。

--- main.py
+++ main.py
@@ -1,27 +1,69 @@
-from flask import Flask, render_template, url_for
+from datetime import timedelta
+from flask import Flask, render_template, url_for, flash, redirect, request, session
+import pymysql
+import jinja2
 
 app = Flask(__name__)
+app.secret_key='12345qwert'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=15)  # 设置会话的持续时间（这里设置为 5 分钟）
+app.config['SESSION_PERMANENT'] = False  # 设置会话在浏览器关闭时过期
 
 @app.route("/")
-@app.route("/index")
+def home():
+    #重定向“/”到登录页面
+    return redirect(url_for("login"))
+
+@app.route('/index')
 def index():
+    if 'username' not in session:
+        return redirect(url_for("login"))
     return render_template("index.html")
 
-@app.route('/user_page/<name>')
-def user_page(name):
-    print( name + ' is a good name.')
-    return render_template("schedule.html")
-    #return name + 'is a good name.
-    
 
-@app.route('/route_test')
-def route_test():
-    print(url_for('index'))
-    print(url_for('user_page',name = 'Sam'))
-    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # 实现登录逻辑
+        username = request.form['username']
+        password = request.form['password']
+
+        user = authenticate_user(username, password)
+        
+        if user:
+            session['username'] = user['username']
+            return render_template("index.html")
+        else:
+           flash("用户名或密码错误,请重新登录")
+    return render_template("login.html")
+
+
+
+def authenticate_user(username, password):
+    #登陆验证
+    connection = pymysql.connect(
+        host='43.140.253.7',
+        user='root',
+        password='Shy990428!',
+        database='shy',
+        port=22704
+    )
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM users WHERE user_name = %s AND password = %s"
+            cursor.execute(sql, (username, password))
+            result = cursor.fetchone()
+            if result:
+                result_dict = {
+                    'username' : result[0],
+                    'password' : result[1]
+                }
+                return result_dict
+    finally:
+        connection.close()
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 
 
---
+++ test.py
@@ -0,0 +1,28 @@
+import pymysql
+
+host = '43.140.253.7'
+user = "root"
+password = "Shy990428!"
+database = "shy"
+port = 22704  # 用您自己的端口号替换
+
+'''try:
+# 连接到 MySQL 服务器
+    connection = pymysql.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+        port=port
+    )
+    print("连接成功！")
+
+except pymysql.Error as e:
+    print(f"连接失败，错误信息：{e}")
+
+finally:
+    # 关闭连接
+    if connection:
+        connection.close()'''
+        
+        
